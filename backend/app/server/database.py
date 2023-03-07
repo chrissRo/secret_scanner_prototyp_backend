7a3a7f8cbddb9335184c1da31416b74637d45f35
christian pritzl <christian.pritzl@outlook.de>
2023-03-07 21:11:31 +0100

    changed mongo-connection-url back to container-version

--- backend/app/server/database.py
+++ backend/app/server/database.py
@@ -7,12 +7,12 @@ from pymongo.errors import ServerSelectionTimeoutError
 load_dotenv()
 
 try:
-    mongo_details = f"mongodb://{os.getenv('MONGODB_USER')}:{os.getenv('MONGODB_PASSWORD')}@localhost:27017" \
+    mongo_details = f"mongodb://{os.getenv('MONGODB_USER')}:{os.getenv('MONGODB_PASSWORD')}@mongodb_container:27017" \
                     f"/?authMechanism=DEFAULT "
 except ServerSelectionTimeoutError as e:
     print("Hostname: mongodb_container not found")
     print("Will try again with localhost")
-    mongo_details = f"mongodb://{os.getenv('MONGODB_USER')}:{os.getenv('MONGODB_PASSWORD')}@mongodb_container:27017" \
+    mongo_details = f"mongodb://{os.getenv('MONGODB_USER')}:{os.getenv('MONGODB_PASSWORD')}@localhost:27017" \
                     f"/?authMechanism=DEFAULT "
 
 db_client = motor.motor_asyncio.AsyncIOMotorClient(mongo_details)
