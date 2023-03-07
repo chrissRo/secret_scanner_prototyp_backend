927aa5f75c7e874a20eb362c0685a6e23cf1f9c0
christian pritzl <christian.pritzl@outlook.de>
2023-03-07 20:01:18 +0100

    add https to connection

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,14 @@ from app.globals.global_config import AvailableScanner
 from app.server.fs_scan_results.fs_scan_results_manager import FSScanResultsManager
 
 if __name__ == "__main__":
-    uvicorn.run("app.server.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "app.server.app:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        ssl_keyfile='../certificate/backend.key',
+        ssl_certfile='../certificate/backend.pem'
+        )
 
     #fs_scan_results_manager = FSScanResultsManager(scanner=AvailableScanner.GITLEAKS, scanner_version='8.15')
     #asyncio.run(fs_scan_results_manager.run())
