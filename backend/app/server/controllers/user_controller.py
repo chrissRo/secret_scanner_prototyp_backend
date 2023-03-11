a1f68aee9f2e2a929427b98a1d999541c62ab945
christian pritzl <christian.pritzl@outlook.de>
2023-03-11 15:21:32 +0100

    add simple logging

--- backend/app/server/controllers/user_controller.py
+++ backend/app/server/controllers/user_controller.py
@@ -1,7 +1,10 @@
+import logging
+
 from fastapi.encoders import jsonable_encoder
 from app.server.database import user_collection
 from app.server.models.user_models.user import UserModel, UserPublicModel
 
+logger = logging.getLogger(__name__)
 
 #####################################
 # GET
@@ -9,9 +12,13 @@ from app.server.models.user_models.user import UserModel, UserPublicModel
 
 async def retrieve_single_user(user_id: str = '', username: str = '') -> UserPublicModel:
     if user_id:
+        logger.debug("Found user with user_id {} in database".format(user_id))
         return await user_collection.find_one({'_id': user_id})
     if username:
+        logger.debug("Found user {} in database".format(username, user_id))
         return await user_collection.find_one({'username': username})
+    else:
+        logger.debug("Did not find user for user_id {} in database".format(user_id))
 
 
 async def retrieve_single_user_private(username: str) -> UserModel:
@@ -22,6 +29,7 @@ async def retrieve_all_user() -> list:
     users = []
     async for user in user_collection.find():
         users.append(user)
+    logger.debug("Found {} users in database".format(len(users)))
     return users
 
 
