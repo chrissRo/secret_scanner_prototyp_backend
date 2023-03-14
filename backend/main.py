11ca188aaa85b85779faae1f03633223972421d3
christian pritzl <christian.pritzl@outlook.de>
2023-03-14 19:15:24 +0100

    add arg-parser options to configure startup

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,22 @@
+import argparse
 import asyncio
+import datetime
 import logging
+import sys
 
 import uvicorn
 
+from app.globals.global_config import AvailableScanner, InputType
+from app.server.fs_scan_results.fs_scan_results_manager import FSScanResultsManager
+from app.server.models.finding_models.finding_model import UploadNewFindingModel
 from app.user.user_manager import UserManager
 from config.config import LoggerConfig
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start-webserver', required=False, help='Start uvicorn webserver', action='store_true')
+    parser.add_argument('--bulk-upload', required=False, help='Start bulk-upload of data', action='store_true')
 
     logging.basicConfig(
         level=LoggerConfig.LOG_LEVEL,
@@ -15,18 +25,35 @@ if __name__ == "__main__":
         format=LoggerConfig.LOG_FORMAT
     )
 
-    logging.debug('Start Application')
+    args = parser.parse_args()
 
-    asyncio.run(UserManager().run())
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
-    uvicorn.run(
-        "app.server.app:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True,
-        log_config='config/uvicornConfig.yml'
+    if args.start_webserver:
+        logging.debug('Starting webserver and application')
+
+        asyncio.run(UserManager().run())
+
+        print("Backend is running ...")
+        uvicorn.run(
+            "app.server.app:app",
+            host="0.0.0.0",
+            port=8000,
+            reload=True,
+            log_config='config/uvicornConfig.yml'
         )
+    else:
+        if args.bulk_upload:
+            logging.debug("Starting file-upload")
 
-    #fs_scan_results_manager = FSScanResultsManager(scanner=AvailableScanner.GITLEAKS, scanner_version='8.15')
-    #asyncio.run(fs_scan_results_manager.run())
-
+            fs_scan_results_manager = FSScanResultsManager()
+            asyncio.run(fs_scan_results_manager.run(meta_data=UploadNewFindingModel(
+                scannerType=AvailableScanner.GITLEAKS,
+                scannerVersion='8.15',
+                inputType=InputType.FileSystem,
+                repositoryPath=".",
+                repositoryName='Manual-import',
+                scanDate=datetime.datetime.now()
+            )))
