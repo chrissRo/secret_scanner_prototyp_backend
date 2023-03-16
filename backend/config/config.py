14447f086fdbb3e18a48ac7050678706c15b9295
christian pritzl <christian.pritzl@outlook.de>
2023-03-16 10:35:12 +0100

    cleanup

--- backend/config/config.py
+++ backend/config/config.py
@@ -5,7 +5,7 @@ class GitleaksConfig:
 
     FS_RAW_INPUT_PATH = 'test/gitleaks_input' # relative to main.py
     FS_RAW_INPUT_FILE_TYPE = '.json'
-    FS_FILE_NAME_MODEL = '<YYYY-MM-DD>__<repository_name>' # 2021-08-21__cp-middleware.json ISO-Format
+    #FS_FILE_NAME_MODEL = '<YYYY-MM-DD>__<repository_name>' # 2021-08-21__cp-middleware.json ISO-Format
 
 class InitialModelValue:
     JUSTIFICATION = 'init'
